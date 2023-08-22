b4a62e993b63a696a74dc6e0ae75c68bfa882a78
dayeon <tjdekdus@naver.com>
2023-08-22 18:16:57 +0900

    slide

--- script/common.js
+++ script/common.js
@@ -1,1 +1,19 @@
 // header, footer JS
+const sub = document.querySelectorAll('.sub')
+const nav = document.querySelectorAll('nav > ul > li')
+const nav_bg = document.querySelector('.nav_bg')
+function sub_func(status){
+    nav_bg.style.display = status
+    for(let i of sub){i.style.display = status}
+}
+sub_func('none')
+for(let i of nav){
+    i.addEventListener('mouseover',function(){
+        sub_func('block')
+        i.lastElementChild.style.backgroundColor = '#fff'
+    })
+    i.addEventListener('mouseout',function(){
+        sub_func('none')
+        i.lastElementChild.style.backgroundColor = 'rgba(255,255,255,0)'
+    })
+}
